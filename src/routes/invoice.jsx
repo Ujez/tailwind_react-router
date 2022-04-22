647532aa507ad04089a917a438f44092f4c655b7
ujez <ujezujez@gmail.com>
2022-04-22 13:27:48 +0100

    added the delete button

--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,7 +1,13 @@
-import { useParams } from "react-router-dom";
-import { getInvoice } from "../data";
+import {
+  useParams,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import { getInvoice, deleteInvoice } from "../data";
 
 export default function Invoice() {
+  let navigate = useNavigate();
+  let location = useLocation();
   let params = useParams();
   let invoice = getInvoice(parseInt(params.invoiceId, 10));
   return (
@@ -11,6 +17,16 @@ export default function Invoice() {
         {invoice.name}: {invoice.number}
       </p>
       <p>Due Date: {invoice.due}</p>
+      <p className="py-3">
+        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+          onClick={() => {
+            deleteInvoice(invoice.number);
+            navigate("/invoices" + location.search);
+          }}
+        >
+          Delete
+        </button>
+      </p>
     </main>
   );
 }
